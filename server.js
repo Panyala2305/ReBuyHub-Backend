0ab6105ae7ca7df0277ac9919188384845536380
Panyala2305 <akashreddy2305@gmail.com>
2025-05-01 11:58:05 -0700

    server.js update

--- server.js
+++ server.js
@@ -40,9 +40,10 @@ const PORT = process.env.PORT || 5000;
 const startServer = async () => {
   try {
     await connectDB();
-    app.listen(PORT, () => {
-      console.log(`🚀 Server running on http://localhost:${PORT}`);
+    app.listen(PORT, '0.0.0.0', () => {
+      console.log(`🚀 Server running on http://0.0.0.0:${PORT}`);
     });
+    
   } catch (error) {
     console.error("❌ Failed to connect to database:", error.message);
     process.exit(1);
